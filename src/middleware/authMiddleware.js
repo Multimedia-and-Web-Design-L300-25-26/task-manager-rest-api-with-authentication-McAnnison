25bf92e6a266a3eb1b23c7c68cbb10d4dea70569
Mensah Anni <mensahanni98@gmail.com>
2026-03-05 04:30:31 +0000

    Implemented user authentication and task management features

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -11,6 +11,25 @@ import User from "../models/User.js";
 
 const authMiddleware = async (req, res, next) => {
   //  implement here
+  try {
+    const authHeader = req.header("Authorization");
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ message: "Access denied. No token provided." });
+    }
+
+    const token = authHeader.replace("Bearer ", "").trim();
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.id).select("-password");
+    if (!user) {
+      return res.status(401).json({message: "Invalid token."})
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Unauthorized." });
+  }
 };
 
 export default authMiddleware;
